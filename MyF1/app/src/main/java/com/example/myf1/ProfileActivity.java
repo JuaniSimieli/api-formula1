b2eccba00e5253ab30ff1dccc90236a7feb0afd8
MarianoLicera <marianolicera3@gmail.com>
2022-06-02 23:09:26 -0300

    added ImagePicker class

--- MyF1/app/src/main/java/com/example/myf1/ProfileActivity.java
+++ MyF1/app/src/main/java/com/example/myf1/ProfileActivity.java
@@ -19,6 +19,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -91,9 +92,13 @@ public class ProfileActivity extends AppCompatActivity {
         profileImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // open gallery
-                Intent openGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(openGallery,1000);
+                ImagePicker.with(ProfileActivity.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        /*.compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)*/
+                        .start();
+                /*Intent openGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGallery,1000);*/
             }   
         });
 
@@ -146,12 +151,8 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1000){
-            if(resultCode == Activity.RESULT_OK){
-                Uri imageUri = data.getData();
-                uploadImageFirebase(imageUri);
-            }
-        }
+        Uri imageUri = data.getData();
+        uploadImageFirebase(imageUri);
     }
 
     private void uploadImageFirebase(Uri imageUri) {
