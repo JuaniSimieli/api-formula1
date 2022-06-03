44f36d92026a7b1f55d33131320e89e9f556c9a5
MarianoLicera <marianolicera3@gmail.com>
2022-06-02 23:18:57 -0300

    removed unnecessary codelines

--- MyF1/app/src/main/java/com/example/myf1/ProfileActivity.java
+++ MyF1/app/src/main/java/com/example/myf1/ProfileActivity.java
@@ -1,16 +1,12 @@
 package com.example.myf1;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,25 +16,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 import java.util.Objects;
 
 
 public class ProfileActivity extends AppCompatActivity {
-    private static final int GALLERY_INTENT_CODE=1023;
     EditText txtProfileName, txtProfileEmail;
     TextView txtProfileNameDefault;
     FirebaseAuth myAuth;
@@ -75,82 +64,47 @@ public class ProfileActivity extends AppCompatActivity {
         userID = Objects.requireNonNull(myAuth.getCurrentUser()).getUid();
 
         DocumentReference documentReference = db.collection("users").document(userID);
-        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                txtProfileNameDefault.setText(value.getString("Nombre"));
-            }
+        documentReference.addSnapshotListener(this, (value, error) -> {
+            assert value != null;
+            txtProfileNameDefault.setText(value.getString("Nombre"));
         });
         StorageReference profileReference = storageReference.child("users/"+userID+"/profile.jpg");
-        profileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                Picasso.get().load(uri).into(profileImage);
-            }
-        });
+        profileReference.getDownloadUrl().addOnSuccessListener(uri -> Picasso.get().load(uri).into(profileImage));
 //TODO: RESOLVER EL ERROR DE LA IMAGEN FIREBASE
-        profileImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ImagePicker.with(ProfileActivity.this)
-                        .crop()	    			//Crop image(Optional), Check Customization for more option
-                        /*.compress(1024)			//Final image size will be less than 1 MB(Optional)
-                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)*/
-                        .start();
-                /*Intent openGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(openGallery,1000);*/
-            }   
+        profileImage.setOnClickListener(view -> {
+            ImagePicker.with(ProfileActivity.this)
+                    .crop()	    			//Crop image(Optional), Check Customization for more option
+                    /*.compress(1024)			//Final image size will be less than 1 MB(Optional)
+                    .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)*/
+                    .start();
         });
 
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                logout(view);
-            }
-        });
+        logoutButton.setOnClickListener(this::logout);
 
-        changePasswordLocal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final EditText resetPassword = new EditText(view.getContext());
-                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(view.getContext());
-                passwordResetDialog.setTitle("¿Desea cambiar la contraseña?");
-                passwordResetDialog.setMessage("Ingrese la nueva contraseña");
-                passwordResetDialog.setView(resetPassword);
+        changePasswordLocal.setOnClickListener(view -> {
+            final EditText resetPassword = new EditText(view.getContext());
+            final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(view.getContext());
+            passwordResetDialog.setTitle("¿Desea cambiar la contraseña?");
+            passwordResetDialog.setMessage("Ingrese la nueva contraseña");
+            passwordResetDialog.setView(resetPassword);
 
-                passwordResetDialog.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //extraer el email y enviar el link para reestablecer
-                        String password = resetPassword.getText().toString();
-                        user.updatePassword(password).addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                Toast.makeText(ProfileActivity.this, "Contraseña cambiada exitosamente!", Toast.LENGTH_SHORT).show();
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(ProfileActivity.this, "Error, no se pudo cambiar la contraseña.", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                });
+            passwordResetDialog.setPositiveButton("Sí", (dialogInterface, i) -> {
+                //extraer el email y enviar el link para reestablecer
+                String password = resetPassword.getText().toString();
+                user.updatePassword(password).addOnSuccessListener(unused -> Toast.makeText(ProfileActivity.this, "Contraseña cambiada exitosamente!", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Toast.makeText(ProfileActivity.this, "Error, no se pudo cambiar la contraseña.", Toast.LENGTH_SHORT).show());
+            });
 
-                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //close
-                    }
-                });
-                passwordResetDialog.create().show();
-            }
+            passwordResetDialog.setNegativeButton("No", (dialogInterface, i) -> {
+                //close
+            });
+            passwordResetDialog.create().show();
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        assert data != null;
         Uri imageUri = data.getData();
         uploadImageFirebase(imageUri);
     }
@@ -158,22 +112,7 @@ public class ProfileActivity extends AppCompatActivity {
     private void uploadImageFirebase(Uri imageUri) {
         // upload image to firebase storage
         StorageReference fileReference = storageReference.child("users/"+userID+"/profile.jpg");
-        fileReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        Picasso.get().load(uri).into(profileImage);
-                    }
-                });
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(ProfileActivity.this, "Error, no se pudo guardar la imagen.", Toast.LENGTH_SHORT).show();
-            }
-        });
+        fileReference.putFile(imageUri).addOnSuccessListener(taskSnapshot -> fileReference.getDownloadUrl().addOnSuccessListener(uri -> Picasso.get().load(uri).into(profileImage))).addOnFailureListener(e -> Toast.makeText(ProfileActivity.this, "Error, no se pudo guardar la imagen.", Toast.LENGTH_SHORT).show());
     }
 
     private void logout(View view){
